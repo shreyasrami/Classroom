9a93fed6b63d0ff341f727a48b03b22f3a5fb7e7
shreyasrami <shreyasrami24.sr@gmail.com>
2020-09-03 18:37:02 +0530

    Add a change to Model relations

--- quiz/models.py
+++ quiz/models.py
@@ -8,10 +8,14 @@ class Quiz(models.Model):
     teacher = models.ForeignKey(Teacher,on_delete=models.CASCADE)
     topic = models.CharField(max_length=30)
     total_marks = models.IntegerField()
+
+    def __str__(self):
+        return self.topic
     
     class Meta:
         verbose_name_plural = 'Quizzes'
-        
+    
+
 class Question(models.Model):
     quiz = models.ForeignKey('Quiz',on_delete=models.CASCADE)
     question_text = models.TextField()
@@ -20,15 +24,19 @@ class Question(models.Model):
     choice3 = models.CharField(max_length=30)
     choice4 = models.CharField(max_length=30)
     correct_choice = models.CharField(max_length=30)
+
     
 
 class Answer(models.Model):
     student = models.ForeignKey(Student,on_delete=models.CASCADE)
-    question = models.OneToOneField('Question',on_delete=models.CASCADE)
+    question = models.ForeignKey('Question',on_delete=models.CASCADE)
     answer = models.CharField(max_length=30)
 
+    
 
 class Result(models.Model):
     student = models.ForeignKey(Student,on_delete=models.CASCADE)
     quiz = models.ForeignKey('Quiz',on_delete=models.CASCADE)
     marks_obtained = models.IntegerField()
+
+    
